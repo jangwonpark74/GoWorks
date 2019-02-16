eff72121507a507f348d7cc8948198d00f005c96
jangwon park <jangwonpark74@gmail.com>
2019-02-16 21:28:42 +0900

    adding Generator goroute example

---
+++ src/fib_chan.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//Generator goroutine based fib func
+func fib(c chan int, n int) {
+	a, b := 0, 1
+	
+	for i:=0; i < n; i++ {
+		a, b = b, a+b
+		c<-a
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int)
+	
+	go fib(c, 10)
+
+	for x := range c {
+		fmt.Println(x)
+	}
+}
