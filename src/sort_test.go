59dffa92f3916c38bdce3706d6a8b06ebfa2e7f8
jang won park <jangwonpark74@gmail.com>
2019-11-04 04:30:48 +0900

    add spec on sort interface

--- src/sort_test.go
+++ src/sort_test.go
@@ -16,9 +16,23 @@ func (p Person) String() string {
 
 type ByAge []Person
 
+'''
+Sort() interface
+
+type Interface interface {
+    // Len is the number of elements in the collection.
+    Len() int
+    // Less reports whether the element with
+    // index i should sort before the element with index j.
+    Less(i, j int) bool
+    // Swap swaps the elements with indexes i and j.
+    Swap(i, j int)
+}
+
+'''
 func (a ByAge) Len() int { return len(a)}
-func (a ByAge) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
 func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age}
+func (a ByAge) Swap(i, j int) { a[i], a[j] = a[j], a[i]}
 
 func Example() {
 	people := []Person {
