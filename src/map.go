e8625567e2efc2d9dc9846dadd1fc65c79da81dc
jang won park <jangwonpark74@gmail.com>
2019-11-03 20:22:13 +0900

    heap test code

--- src/map.go
+++ src/map.go
@@ -1,12 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 	var m map[string]int
 	m = make(map[string]int)
 
 	m["key"] = 42
 	elem, _ := m["key"]
-	print(elem)
+	fmt.Println(elem)
 
 	delete(m, "key")
+
+	monthdays := map[string]int{
+		"Jan":31, "Feb":28, "Mar":31,
+		"Apr":30, "May":31, "Jun":30,
+		"Jul":31, "Aug":31, "Sep":30,
+		"Oct":31, "Nov":30, "Dec":31,
+	}
+
+	year_day := 0
+	for k,v :=range monthdays {
+		fmt.Println(k,v)
+	 	year_day += v	
+	}
+	fmt.Println("Days in a year is :", year_day)
 }
