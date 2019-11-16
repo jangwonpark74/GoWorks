eb86c32b71b437bd8f126523aa8826fbe683e6b6
jang won park <jangwonpark74@gmail.com>
2019-11-16 09:33:05 +0900

    hello_world test

---
+++ src/hello_world/hello_world.go
@@ -0,0 +1,7 @@
+package hello_world
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello, World!")
+}
---
+++ src/hello_world/hello_world_test.go
@@ -0,0 +1,10 @@
+package hello_world_test
+
+import (
+	"hello_world"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	hello_world.Hello()
+}
